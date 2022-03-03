1ee76164c621e3c00a22fa79a2ae108e77e3d81e
JayashriYS <jayu.gurav@gmail.com>
2022-03-04 01:06:14 +0530

    "second Change of Program"

--- src/sample/git/Demo.java
+++ src/sample/git/Demo.java
@@ -1,9 +1,10 @@
-package sample.git;
+package sample;
 
 public class Demo
 {
  public static void main(String[] args) 
  {
 	System.out.println("Hello");
+	System.out.println("All the Best");
 }
 }
